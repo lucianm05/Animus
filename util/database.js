5328f5d217624515aadc314b3be1b32406d142fa
Lucian George MANEA <lucianmg05@gmail.com>
2021-05-23 16:54:57 +0300

    change db password

--- util/database.js
+++ util/database.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize').Sequelize;
 
-const sequelize = new Sequelize('petshop', 'root', 'zvenkolucian2001', { dialect: 'mysql', host: 'localhost'});
+const sequelize = new Sequelize('petshop', 'root', '05PH04ZVKANIMUS2001', { dialect: 'mysql', host: 'localhost'});
 
 module.exports = sequelize;

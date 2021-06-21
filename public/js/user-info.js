f8c43933cdb502ced4defdbfe39e531fdfc8dcba
Lucian George MANEA <lucianmg05@gmail.com>
2021-06-21 09:52:10 +0300

    updated user panel

--- public/js/user-info.js
+++ public/js/user-info.js
@@ -2,9 +2,8 @@ const userInfoPanel = document.getElementById('changeUserInfo');
 
 const showConfirmPassword = (event) => {
   const target = event.target;
-  console.log(target.tagName);
 
-  if(target.tagName === 'svg' || target.tagName === 'path') {
+  if(target.tagName === 'svg') {
     const confirmPasswordDiv = target.parentElement.parentElement.parentElement.nextElementSibling;
     if(confirmPasswordDiv.style.display === 'none') {
       confirmPasswordDiv.style.display = 'block';
@@ -12,8 +11,14 @@ const showConfirmPassword = (event) => {
       confirmPasswordDiv.style.display = 'none';
     }
   } else if(target.tagName === 'BUTTON') {
-    console.log(target.parentElement.parentElement);
     const confirmPasswordDiv = target.parentElement.parentElement.nextElementSibling;
+    if(confirmPasswordDiv.style.display === 'none') {
+      confirmPasswordDiv.style.display = 'block';
+    } else if (confirmPasswordDiv.style.display === 'block') {
+      confirmPasswordDiv.style.display = 'none';
+    }
+  } else if(target.tagName === 'path') {
+    const confirmPasswordDiv = target.parentElement.parentElement.parentElement.parentElement.nextElementSibling;
     if(confirmPasswordDiv.style.display === 'none') {
       confirmPasswordDiv.style.display = 'block';
     } else if (confirmPasswordDiv.style.display === 'block') {

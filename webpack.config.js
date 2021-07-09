3bc5ade4e0c0f77c9a1150c8148780838d4ceaf9
Lucian George MANEA <lucianmg05@gmail.com>
2021-07-09 12:41:13 +0300

    fixed

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-  entry: './js/service-worker.js',
+  entry: './js/script.js',
   target: 'node',
   externals: [nodeExternals()],
   module: {
@@ -14,7 +14,7 @@ module.exports = {
   },
   output: {
     path: path.join(process.cwd(), 'public', 'js'),
-    filename: 'service-worker.js',
+    filename: 'script.js',
   },
   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
 };
